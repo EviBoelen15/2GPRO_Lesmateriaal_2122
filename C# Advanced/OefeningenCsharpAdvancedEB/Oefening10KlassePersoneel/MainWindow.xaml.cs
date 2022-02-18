ccec5299c6533cc05622d308ffd0b1513c02c198
EviBoelen15 <evi.boelen@outlook.com>
2022-02-18 10:58:43 +0100

    C# advanced oefening 10

---
+++ C# Advanced/OefeningenCsharpAdvancedEB/Oefening10KlassePersoneel/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oefening10KlassePersoneel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Personeel personeel; 
+        MessageBoxResult result;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            result = MessageBox.Show("Wil je een lege of nieuwe gebruiker toevoegen", "Personeel", MessageBoxButton.YesNo);
+            if(result.Equals(MessageBoxResult.Yes))
+            {
+                personeel = new Personeel();
+                VulNieuwePersoneel(personeel);
+            }   
+        }
+
+        private void VulNieuwePersoneel(Personeel personeel)
+        {
+            txtNaam.Text = personeel.Naam;
+            txtVoornaam.Text = personeel.Voornaam;
+            txtStartjaar.Text = personeel.Startjaar.ToString();
+            cbGeslacht.SelectedItem = personeel.Geslacht;
+            txtResultaat.Text = personeel.InformatieVolledig();
+        }
+
+        private void btnVerhoog_Click(object sender, RoutedEventArgs e)
+        {
+            if(personeel.Beoordelingscijfer <= 10)
+            {
+                personeel.VerhoogBeoordeling();
+                txtResultaat.Text = personeel.InformatieVolledig();
+            }
+            else
+            {
+                MessageBox.Show("Max beoordeling bereikt");
+            }
+            
+        }
+
+        private void btnVerlaag_Click(object sender, RoutedEventArgs e)
+        {
+            if (personeel.Beoordelingscijfer > 0)
+            {
+                personeel.VerlaaggBeoordeling();
+                txtResultaat.Text = personeel.InformatieVolledig();
+            }
+            else
+            {
+                MessageBox.Show("Min beoordeling bereikt");
+            }
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            personeel = new Personeel(txtNaam.Text, txtVoornaam.Text, cbGeslacht.SelectedItem.ToString(), 0, int.Parse(txtStartjaar.Text));
+        }
+
+
+    }
+}
---
+++ C# Advanced/OefeningenCsharpAdvancedEB/Oefening10KlassePersoneel/Personeel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening10KlassePersoneel
+{
+    public class Personeel
+    {
+        public string Naam { get; set; }
+        public string Voornaam { get; set; }
+        public string Geslacht { get; set; }
+        public int Beoordelingscijfer { get; set; }
+        public int Startjaar { get; set; }
+
+
+        public int Aantaldienstjaren { get; }
+        public string Geslachtstekst { get; }
+        public decimal Premie { get; set; }
+
+
+        public Personeel()
+        {
+            Naam = "";
+            Voornaam = "";
+            Geslacht = "M";
+            Beoordelingscijfer = 0;
+            Startjaar = 2000;
+            Aantaldienstjaren = 0;
+            Premie = BerekenPremie(0, 0);
+        }
+        public Personeel(string naam, string voornaam, string geslacht, int beoordelingscijver, int startjaar)
+        {
+            Naam = naam;
+            Voornaam = voornaam;
+            Geslacht = (geslacht == "M") ? "Mannelijk" : "Vrouwelijk";
+            Beoordelingscijfer = beoordelingscijver;
+            Startjaar = startjaar;
+            Aantaldienstjaren = DateTime.Now.Year - startjaar;
+            Premie = BerekenPremie(Beoordelingscijfer, Aantaldienstjaren);
+        }
+
+        private decimal BerekenPremie(int beoordelingscijfer, int dienstjaren)
+        {
+            decimal premie;
+            decimal startbedrag = 500;
+            decimal maandPremie = 20;
+
+            if(beoordelingscijfer < 5)
+            {
+                startbedrag = startbedrag / 2;
+            } 
+            else if (beoordelingscijfer == 7 || beoordelingscijfer == 8)
+            {
+                startbedrag = startbedrag + (startbedrag * 2);
+            }
+            else
+            {
+                startbedrag = startbedrag * 2;
+            }
+
+            premie = startbedrag + (dienstjaren * maandPremie);
+            return premie;
+        }
+        public void VerhoogBeoordeling()
+        {
+            Beoordelingscijfer++;
+            Premie = BerekenPremie(Beoordelingscijfer, Aantaldienstjaren);
+        }
+        public void VerlaaggBeoordeling()
+        {
+            Beoordelingscijfer--;
+            Premie = BerekenPremie(Beoordelingscijfer, Aantaldienstjaren);
+        }
+        public string InformatieVolledig()
+        {
+            string resultaat = $"Personeelslid {Naam} {Voornaam} \nGeslacht : {Geslacht} \n" +
+                $"Aantal dienstjaren : {Aantaldienstjaren} \n" +
+                $"Beoordelingscijfer : {Beoordelingscijfer} \n" +
+                $"Premie : {Premie}";
+            return resultaat;
+        }
+    }
+}
