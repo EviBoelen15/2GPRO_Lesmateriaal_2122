3c36e52a6341bfcb6af10f8fe7d1a7551ab89d68
EviBoelen15 <evi.boelen@outlook.com>
2022-03-08 11:37:41 +0100

    abdulkadir c# advanced

--- C# Advanced/AbdulkadirCAdvanced/C#Advanced/23-29/CS_ADO_NET/CS_Oefening_027/ArtikelUitCategorie.sql
+++
@@ -1,8 +0,0 @@
--- === STORED PROCEDURE ===
-CREATE OR ALTER PROCEDURE [dbo].[ArtikelUitCategorie]
-    @CatID smallint
-AS
-SELECT artikel, omschrijving, verkoopprijs
-FROM artikel
-WHERE cat_id = @CatID
-ORDER BY artikel;
