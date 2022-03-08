0e2d672c89b58037b2316cf021aaab206d81ccaa
EviBoelen15 <evi.boelen@outlook.com>
2022-03-08 11:37:21 +0100

    Abdulkadir C# Advanced map 2019-2020

---
+++ C# Advanced/AbdulkadirCAdvanced/C#Advanced/ClassLibraryBewerking/ClassLibraryBewerking/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryBewerking
+{
+    public class Bewerking
+    {
+        public float Som(float x, float y)
+        {
+            return x + y;
+        }
+
+        public float Min(float x, float y)
+        {
+            return x - y;
+        }
+
+        public float Maal(float x, float y)
+        {
+            return x * y;
+        }
+    }
+}
